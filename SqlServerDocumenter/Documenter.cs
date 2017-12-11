8a7bd85bcfd10150fa12ce24f19b3cd89c97ec9a
Abrahán Martínez Martín <ammartin@stdmultiopcion.es>
2017-12-11 18:26:19 +0100

    Add functionality to interface IDocumenter

--- SqlServerDocumenter/Documenter.cs
+++ SqlServerDocumenter/Documenter.cs
@@ -1,8 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
+using SqlServerDocumenter.Entities;
 
 namespace SqlServerDocumenter
 {
-    public class Documenter : IDocumenter
-    {
-    }
+	/// <inheritdoc />
+	public class SqlDocumenter : IDocumenter
+	{
+		/// <inheritdoc />
+		public DocumentedDatabase GetDatabase(string serverName, string databaseName)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public IEnumerable<DocumentedDatabase> GetDatabases(string serverName)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public IEnumerable<DocumentedServer> GetServers()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public DocumentedStoredProcedure GetStoredProcedure(string serverName, string databaseName, string schema, string storedProcedureName)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public IEnumerable<DocumentedSimpleObject> GetStoredProcedures(string serverName, string databaseName)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public DocumentedTable GetTable(string serverName, string databaseName, string schema, string tableName)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public IEnumerable<DocumentedSimpleObject> GetTables(string serverName, string databaseName)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public DocumentedView GetView(string serverName, string databaseName, string schema, string viewName)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public IEnumerable<DocumentedSimpleObject> GetViews(string serverName, string databaseName)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public DocumentedDatabase SaveDatabase(DocumentedDatabase database)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public DocumentedStoredProcedure SaveStoredProcedure(DocumentedStoredProcedure procedure)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public DocumentedTable SaveTable(DocumentedTable table)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public DocumentedView SaveView(DocumentedView view)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
--- SqlServerDocumenter/IDocumenter.cs
+++ SqlServerDocumenter/IDocumenter.cs
@@ -1,10 +1,116 @@
-﻿using System;
+﻿using SqlServerDocumenter.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SqlServerDocumenter
 {
-    public interface IDocumenter
-    {
-    }
+	/// <summary>
+	/// Allows read database objects and modify their desctiptions.
+	/// </summary>
+	public interface IDocumenter
+	{
+		/// <summary>
+		/// Allows get the servers that will be consulted to read the databases.
+		/// </summary>
+		/// <returns>A list of servers.</returns>
+		IEnumerable<DocumentedServer> GetServers();
+
+		/// <summary>
+		/// Read the databases of a server.
+		/// </summary>
+		/// <param name="serverName">Name of the server.</param>
+		/// <returns>List of databases.</returns>
+		IEnumerable<DocumentedDatabase> GetDatabases(string serverName);
+
+		/// <summary>
+		/// Read one database.
+		/// </summary>
+		/// <param name="serverName">Name of the server.</param>
+		/// <param name="databaseName">Name of the database.</param>
+		/// <returns></returns>
+		DocumentedDatabase GetDatabase(string serverName, string databaseName);
+
+		/// <summary>
+		/// Save the description of a database.
+		/// </summary>
+		/// <param name="database">Database data.</param>
+		/// <returns>Actualized database data.</returns>
+		DocumentedDatabase SaveDatabase(DocumentedDatabase database);
+
+		/// <summary>
+		/// Read the tables of a database.
+		/// </summary>
+		/// <param name="serverName">Name of the server.</param>
+		/// <param name="databaseName">Name of the database.</param>
+		/// <returns>List of tables of the database</returns>
+		IEnumerable<DocumentedSimpleObject> GetTables(string serverName, string databaseName);
+
+		/// <summary>
+		/// Read one table of a database.
+		/// </summary>
+		/// <param name="serverName">Name of the server.</param>
+		/// <param name="databaseName">Name of the database.</param>
+		/// <param name="schema">Schema of the table.</param>
+		/// <param name="tableName">Name of the table.</param>
+		/// <returns>Table readed.</returns>
+		DocumentedTable GetTable(string serverName, string databaseName, string schema, string tableName);
+
+		/// <summary>
+		/// Save the description of the table.
+		/// </summary>
+		/// <param name="table">Table to save.</param>
+		/// <returns>Table reloaded.</returns>
+		DocumentedTable SaveTable(DocumentedTable table);
+
+		/// <summary>
+		/// Read the view of a database.
+		/// </summary>
+		/// <param name="serverName">Name of the server.</param>
+		/// <param name="databaseName">Name of the database.</param>
+		/// <returns>List of view of the database.</returns>
+		IEnumerable<DocumentedSimpleObject> GetViews(string serverName, string databaseName);
+
+		/// <summary>
+		/// Read one view of a database.
+		/// </summary>
+		/// <param name="serverName">Name of the server.</param>
+		/// <param name="databaseName">Name of the database.</param>
+		/// <param name="schema">Schema of the view.</param>
+		/// <param name="viewName">Name of the view.</param>
+		/// <returns>View readed.</returns>
+		DocumentedView GetView(string serverName, string databaseName, string schema, string viewName);
+
+		/// <summary>
+		/// Save the description of the view.
+		/// </summary>
+		/// <param name="view">View to save.</param>
+		/// <returns>View reloaded.</returns>
+		DocumentedView SaveView(DocumentedView view);
+
+		/// <summary>
+		/// Read the stored procedures of a database.
+		/// </summary>
+		/// <param name="serverName">Name of the server.</param>
+		/// <param name="databaseName">Name of the database.</param>
+		/// <returns>List of stored procedures of the database.</returns>
+		IEnumerable<DocumentedSimpleObject> GetStoredProcedures(string serverName, string databaseName);
+
+		/// <summary>
+		/// Read one procedure of a database.
+		/// </summary>
+		/// <param name="serverName">Name of the server.</param>
+		/// <param name="databaseName">Name of the database.</param>
+		/// <param name="schema">Schema of the procedure.</param>
+		/// <param name="storedProcedureName">Stored procedure name.</param>
+		/// <returns>Procedure readed.</returns>
+		DocumentedStoredProcedure GetStoredProcedure(string serverName, string databaseName, string schema, string storedProcedureName);
+
+		/// <summary>
+		/// Save the description of the procedure.
+		/// </summary>
+		/// <param name="procedure">Procedure to save.</param>
+		/// <returns>Procedure reloaded.</returns>
+		DocumentedStoredProcedure SaveStoredProcedure(DocumentedStoredProcedure procedure);
+	}
 }
